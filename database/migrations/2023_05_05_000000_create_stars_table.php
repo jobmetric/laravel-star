ffb39aeb9c900a6c7469a904b1e3a79c3e6d0e65
majid mohammadian <majeedmohammadian@gmail.com>
2024-05-13 22:58:46 +0330

    add migration

---
+++ database/migrations/2023_05_05_000000_create_stars_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create(config('star.tables.star'), function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')->index()->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+
+            $table->morphs('starable');
+
+            $table->unsignedTinyInteger('star')->default(0);
+
+            $table->timestamps();
+        });
+
+        cache()->forget('stars');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(config('star.tables.star'));
+
+        cache()->forget('stars');
+    }
+};
